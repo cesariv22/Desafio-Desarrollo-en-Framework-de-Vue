85a34ca4bbf9ec97152c5477b1d1352dc0a92178
Cesar Riveros <c-riveros@hotmail.com>
2022-04-05 22:59:51 -0400

    finished challenge

---
+++ components/Card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+      <b-card class="text-center display-4">Elenco Rick and Morty</b-card>
+    <b-container>
+      <b-row>
+        <b-col
+          v-for="personaje in cardPersonajes"
+          :key="personaje.id"
+          cols="12"
+          md="4"
+        >
+          <b-card
+            bg-variant="dark"
+            text-variant="white"
+            :title="personaje.name" 
+            :img-src="personaje.image"
+            :img-alt="personaje.name"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2 card-element"
+          >
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardNuxt',
+  data() {
+    return {
+      cardPersonajes: [],
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      try {
+        const req = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await req.json()
+        this.cardPersonajes = data.results
+      } catch (error) {
+        error(error)
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.card-element {
+    min-height: 21.5rem;
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Card />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
